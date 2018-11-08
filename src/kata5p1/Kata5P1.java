778afe5e695d303a7f2ce4fa510b312a548a7f21
Usuario <Usuario@10.22.148.90>
2018-11-08 09:31:14 +0000

    Kata5P1 versión 2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,12 +3,14 @@ package kata5p1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import static kata5p1.CrearTabla.createNewTable;
 
 public class Kata5P1 {
 
     public static void main(String[] args) {
        SelectApp app = new SelectApp();
         app.selectAll();
+        createNewTable();
     }
     
     private static void connect() {
